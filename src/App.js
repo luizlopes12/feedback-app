e6bb200206e86e2da3ac5470aa359035fcd9c87d
Luiz Lopes <luizlopes12@outlook.com>
2022-01-09 13:27:48 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import React from 'react'
 function App(){
+    const title = 'Blog Post'
+    const body = 'This is my blog post'
+    const comments = [
+        {id: 1, text: 'Comment 1'},
+        {id: 2, text: 'Comment 2'},
+        {id: 3, text: 'Comment 3'}
+    ]
     return (
-        <>
-        <h1>Hello from the app component</h1>
-        <p>Opa</p>
-        </>
+        <div className='container'>
+        <h1>{title}</h1>
+        <p>{body}</p>
+        <div className='comments'>
+        <h3>
+            Comments ({comments.length})
+        </h3>
+        <ul>
+            {comments.map((comment, index)=>(
+                <li key={index}>{comment.text}</li>
+            ))}
+        </ul>
+
+        </div>
+        </div>
     )
 }
 export default App;
