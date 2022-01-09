999117dec383b23cc89236bc8ec7303d63cbd399
Luiz Lopes <luizlopes12@outlook.com>
2022-01-09 14:25:58 -0300

    utilizando states para os itens da lista

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import PropTypes from 'prop-types';
-function Header({text}) {
+function Header({text, bgColor, textColor}) {
+    const headerStyles = {
+        backgroundColor: bgColor,
+        color: textColor,
+    }
     return (
-        <header>
+        <header style={headerStyles}>
             <div className="container">
                 <h2>{text}</h2>
             </div>
@@ -11,7 +15,9 @@ function Header({text}) {
 }
 //deixa uma configuração de props default se elas não forem inputadas no component
 Header.defaultProps = {
-    text: 'Feedback UI'
+    text: 'Feedback UI',
+    bgColor: 'rgba(0,0,0,0.4)',
+    textColor: '#ff6a95',
 }
 //Impede de que o valor passado pelas props seja um tipo de dado diferente do esperado
 /*
