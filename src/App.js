3a466e6d7ef583ef8bb81ad8b7c2ef64fe3bf83a
Luiz Lopes <luizlopes12@outlook.com>
2022-01-09 13:47:16 -0300

    Criando o header e anotando alguns conceitos

--- src/App.js
+++ src/App.js
@@ -1,28 +1,14 @@
 import React from 'react'
+import Header from './components/Header'
 function App(){
-    const title = 'Blog Post'
-    const body = 'This is my blog post'
-    const comments = [
-        {id: 1, text: 'Comment 1'},
-        {id: 2, text: 'Comment 2'},
-        {id: 3, text: 'Comment 3'}
-    ]
     return (
+        <>
+        <Header />
         <div className='container'>
-        <h1>{title}</h1>
-        <p>{body}</p>
-        <div className='comments'>
-        <h3>
-            Comments ({comments.length})
-        </h3>
-        <ul>
-            {comments.map((comment, index)=>(
-                <li key={index}>{comment.text}</li>
-            ))}
-        </ul>
+            <h1>My App</h1>
+        </div>
+        </>
 
-        </div>
-        </div>
     )
 }
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import PropTypes from 'prop-types';
+function Header({text}) {
+    return (
+        <header>
+            <div className="container">
+                <h2>{text}</h2>
+            </div>
+        </header>
+    )
+}
+//deixa uma configuração de props default se elas não forem inputadas no component
+Header.defaultProps = {
+    text: 'Feedback UI'
+}
+//Impede de que o valor passado pelas props seja um tipo de dado diferente do esperado
+Header.PropTypes = {
+    text: PropTypes.string,
+    //Aqui por exemplo a prop text só recebe valores em string, qualquer outro é rejeitado 
+}
+export default Header
