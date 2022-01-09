de07a6b4452050aa804657814504a0e1d5c2a15f
Luiz Lopes <luizlopes12@outlook.com>
2022-01-09 15:17:55 -0300

    Update FeedbackItem.jsx

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,9 +1,16 @@
-import {} from 'react-icons'
+import {FaTimes} from 'react-icons/fa'
 import Card from './shared/Card'
 function FeedbackItem({item}) {
+    const handleClick = (id) =>{
+        console.log(id)
+    }
+
     return (
         <Card>
             <div className="num-display">{item.rating}</div>
+            <button onClick={()=> handleClick(item.id)} className="close">
+                <FaTimes color='purple'/>
+            </button>
             <div className="text-display">{item.text}</div>
         </Card>
     )
