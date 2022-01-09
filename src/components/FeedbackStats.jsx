b37157704bd1853306defcc676cfa1b89e5bdfb1
Luiz Lopes <luizlopes12@outlook.com>
2022-01-09 15:45:18 -0300

    Formatando os documentos
    formatando tudo com prettier

--- src/components/FeedbackStats.jsx
+++ src/components/FeedbackStats.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
-import FeedbackItem from './FeedbackItem'
+import React from "react";
+import FeedbackItem from "./FeedbackItem";
 
-function FeedbackStats({feedback}) {
-    //calculate rating avg
-    let avg = feedback.reduce((acc, cur)=>{return acc + cur.rating},0)/feedback.length
-    avg = avg.toFixed(1).replace(/[.,]0$/, '')
-    return (
-        <div className='feedback-stats'>
-            <h4>{feedback.length} Reviews</h4>
-            <h4>Average rating: {isNaN(avg)? 0 : avg}</h4>
-        </div>
-    )
+function FeedbackStats({ feedback }) {
+  //calculate rating avg
+  let avg =
+    feedback.reduce((acc, cur) => {
+      return acc + cur.rating;
+    }, 0) / feedback.length;
+  avg = avg.toFixed(1).replace(/[.,]0$/, "");
+  return (
+    <div className="feedback-stats">
+      <h4>{feedback.length} Reviews</h4>
+      <h4>Average rating: {isNaN(avg) ? 0 : avg}</h4>
+    </div>
+  );
 }
 
-export default FeedbackStats
+export default FeedbackStats;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,28 @@
-import React from 'react'
-import PropTypes from 'prop-types';
-function Header({text, bgColor, textColor}) {
-    const headerStyles = {
-        backgroundColor: bgColor,
-        color: textColor,
-    }
-    return (
-        <header style={headerStyles}>
-            <div className="container">
-                <h2>{text}</h2>
-            </div>
-        </header>
-    )
+import React from "react";
+import PropTypes from "prop-types";
+function Header({ text, bgColor, textColor }) {
+  const headerStyles = {
+    backgroundColor: bgColor,
+    color: textColor,
+  };
+  return (
+    <header style={headerStyles}>
+      <div className="container">
+        <h2>{text}</h2>
+      </div>
+    </header>
+  );
 }
 //deixa uma configuração de props default se elas não forem inputadas no component
 Header.defaultProps = {
-    text: 'Feedback UI',
-    bgColor: 'rgba(0,0,0,0.4)',
-    textColor: '#ff6a95',
-}
+  text: "Feedback UI",
+  bgColor: "rgba(0,0,0,0.4)",
+  textColor: "#ff6a95",
+};
 //Impede de que o valor passado pelas props seja um tipo de dado diferente do esperado
 /*
 Header.PropTypes = {
     text: PropTypes.string
     //Aqui por exemplo a prop text só recebe valores em string, qualquer outro é rejeitado 
 }*/
-export default Header
+export default Header;
