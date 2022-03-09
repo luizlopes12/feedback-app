1c8aeda51a8e7fff215cf1286574d341e70c1c93
Luiz Lopes <luizlopes12@outlook.com>
2022-03-09 11:22:20 -0300

    adicionando verificação de input

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,24 +1,40 @@
 import React,{useState} from 'react';
 import Card from './shared/Card';
+import RatingSelect from './RatingSelect';
 import Button from './shared/Button';
 function FeedbackForm() {
     const [text, setText] = useState('')
+    const [rating, setRating] = useState(10)
+    const [btnDisabled, setBtnDisabled] = useState(true)
+    const [message, setMessage] = useState('')
+
     const handleTextChange = (e) =>{
+        if(text === ''){
+            setBtnDisabled(true)
+            setMessage(null)
+        }else if(text !== '' && text.trim().length <= 10){
+            setMessage('Text must be at least 10 caracters')
+            setBtnDisabled(true)
+        }else{
+            setMessage(null)
+            setBtnDisabled(false)
+        }
         setText(e.target.value)
     }
     return (
         <Card>
             <form>
                 <h2>How would you rate your service with us?</h2>
-                    {/* Rating select component */}
+                    <RatingSelect/>
                     <div className="input-group">
                         <input 
                         type="text" 
                         value={text} 
                         onChange={handleTextChange} 
                         placeholder='Write a review'/>
-                        <Button type="submit" version='primary'>Send</Button>
+                        <Button isDisabled={btnDisabled} type="submit" version='primary'>Send</Button>
                     </div>
+                    {message&& <div className='message'>{message}</div>}
             </form>
         </Card>
     );
